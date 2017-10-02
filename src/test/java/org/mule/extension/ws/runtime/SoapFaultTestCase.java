eef407a3a1ce29f38cdf436804437c7bade50c3a
Rodrigo Merino <elrodro83@users.noreply.github.com>
2017-10-02 18:10:52 +0200

    MULE-10519: Move all exception classes away from MessagingException (#51)

--- src/test/java/org/mule/extension/ws/runtime/SoapFaultTestCase.java
+++ src/test/java/org/mule/extension/ws/runtime/SoapFaultTestCase.java
@@ -6,6 +6,7 @@
  */
 package org.mule.extension.ws.runtime;
 
+import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
@@ -13,20 +14,20 @@ import static org.hamcrest.Matchers.isOneOf;
 import static org.junit.Assert.assertThat;
 import static org.mule.extension.ws.AllureConstants.WscFeature.WSC_EXTENSION;
 import static org.mule.runtime.soap.api.SoapVersion.SOAP11;
-import static org.mule.tck.junit4.matcher.ErrorTypeMatcher.errorType;
+
 import org.mule.extension.ws.AbstractSoapServiceTestCase;
-import org.mule.runtime.api.message.Error;
-import org.mule.runtime.core.api.exception.EventProcessingException;
+import org.mule.functional.api.exception.ExpectedError;
 import org.mule.runtime.soap.api.exception.BadRequestException;
 import org.mule.runtime.soap.api.exception.SoapFaultException;
-import org.mule.tck.junit4.matcher.ErrorTypeMatcher;
+
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
 
 import io.qameta.allure.Description;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Stories;
 import io.qameta.allure.Story;
-import org.hamcrest.Matchers;
-import org.junit.Test;
 
 @Feature(WSC_EXTENSION)
 @Stories({@Story("Operation Execution"), @Story("Soap Fault")})
@@ -38,6 +39,9 @@ public class SoapFaultTestCase extends AbstractSoapServiceTestCase {
   private static final String SOAP_FAULT = "SOAP_FAULT";
   private static final String BAD_REQUEST = "BAD_REQUEST";
 
+  @Rule
+  public ExpectedError expected = ExpectedError.none();
+
   @Override
   protected String getConfigurationFile() {
     return "config/fail.xml";
@@ -46,52 +50,62 @@ public class SoapFaultTestCase extends AbstractSoapServiceTestCase {
   @Test
   @Description("Consumes an operation that throws a SOAP Fault and expects a Soap Fault Exception")
   public void failOperation() throws Exception {
-    EventProcessingException me = flowRunner(FAIL_FLOW).withPayload(testValues.getFailRequest()).runExpectingException();
-    Error error = me.getEvent().getError().get();
+    expected.expectErrorType("WSC", SOAP_FAULT);
+    expected.expectCause(allOf(instanceOf(SoapFaultException.class),
+                               new TypeSafeMatcher<SoapFaultException>(SoapFaultException.class) {
 
-    assertThat(error.getErrorType(), is(errorType("WSC", SOAP_FAULT)));
+                                 @Override
+                                 protected boolean matchesSafely(SoapFaultException sf) {
+                                   // Receiver is for SOAP12. Server is for SOAP11
+                                   assertThat(sf.getFaultCode().getLocalPart(), isOneOf("Server", "Receiver"));
+                                   assertThat(sf.getMessage(), containsString("Fail Message"));
 
-    Throwable causeException = error.getCause();
-    assertThat(causeException, instanceOf(SoapFaultException.class));
-    SoapFaultException sf = (SoapFaultException) causeException;
+                                   return true;
+                                 }
 
-    // Receiver is for SOAP12. Server is for SOAP11
-    assertThat(sf.getFaultCode().getLocalPart(), isOneOf("Server", "Receiver"));
-    assertThat(sf.getMessage(), containsString("Fail Message"));
+                                 @Override
+                                 public void describeTo(org.hamcrest.Description description) {}
+                               }));
+
+    flowRunner(FAIL_FLOW).withPayload(testValues.getFailRequest()).run();
   }
 
   @Test
   @Description("Consumes an operation that does not exist and throws a SOAP Fault because of it and asserts the thrown exception")
   public void noExistentOperation() throws Exception {
     String badRequest = "<con:noOperation xmlns:con=\"http://service.soap.service.mule.org/\"/>";
-    EventProcessingException me = flowRunner(FAIL_FLOW).withPayload(badRequest).runExpectingException();
 
-    Error error = me.getEvent().getError().get();
-
-    assertThat(error.getErrorType(), Matchers.is(ErrorTypeMatcher.errorType("WSC", SOAP_FAULT)));
-    assertThat(error.getCause(), instanceOf(SoapFaultException.class));
-    SoapFaultException sf = (SoapFaultException) error.getCause();
-
-    // Sender is for SOAP12. Client is for SOAP11
-    assertThat(sf.getFaultCode().getLocalPart(), isOneOf("Client", "Sender"));
-
-    String errorMessage;
+    expected.expectErrorType("WSC", SOAP_FAULT);
     if (soapVersion.equals(SOAP11)) {
-      errorMessage = "{http://service.soap.service.mule.org/}noOperation was not recognized";
+      expected.expectMessage(containsString("{http://service.soap.service.mule.org/}noOperation was not recognized"));
     } else {
-      errorMessage = "Unexpected wrapper element {http://service.soap.service.mule.org/}noOperation";
+      expected.expectMessage(containsString("Unexpected wrapper element {http://service.soap.service.mule.org/}noOperation"));
     }
-    assertThat(sf.getMessage(), containsString(errorMessage));
+    expected.expectCause(allOf(instanceOf(SoapFaultException.class),
+                               new TypeSafeMatcher<SoapFaultException>(SoapFaultException.class) {
+
+                                 @Override
+                                 protected boolean matchesSafely(SoapFaultException sf) {
+                                   // Sender is for SOAP12. Client is for SOAP11
+                                   assertThat(sf.getFaultCode().getLocalPart(), isOneOf("Client", "Sender"));
+
+                                   return true;
+                                 }
+
+                                 @Override
+                                 public void describeTo(org.hamcrest.Description description) {}
+                               }));
+
+    flowRunner(FAIL_FLOW).withPayload(badRequest).run();
   }
 
   @Test
   @Description("Consumes an operation with a body that is not a valid XML")
   public void echoBodyIsNotValidXml() throws Exception {
-    EventProcessingException me = flowRunner(FAIL_FLOW).withPayload("not a valid XML file").runExpectingException();
-    Error error = me.getEvent().getError().get();
+    expected.expectErrorType("WSC", BAD_REQUEST);
+    expected.expectMessage(is("Error consuming the operation [fail], the request body is not a valid XML"));
+    expected.expectCause(instanceOf(BadRequestException.class));
 
-    assertThat(error.getErrorType(), is(errorType("WSC", BAD_REQUEST)));
-    assertThat(error.getCause(), instanceOf(BadRequestException.class));
-    assertThat(error.getCause().getMessage(), is("Error consuming the operation [fail], the request body is not a valid XML"));
+    flowRunner(FAIL_FLOW).withPayload("not a valid XML file").run();
   }
 }
