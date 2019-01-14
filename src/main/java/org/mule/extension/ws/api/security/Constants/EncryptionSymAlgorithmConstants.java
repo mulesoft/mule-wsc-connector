e89f8b86c0a3b637f6c3b98cb5869349cd598a09
Juan Baca Paunero <Bardammu@users.noreply.github.com>
2019-01-14 16:44:37 -0300

    MULE-16100: WSC:  allow signature security strategy to specify the signature algorithm (#102)

---
+++ src/main/java/org/mule/extension/ws/api/security/Constants/EncryptionSymAlgorithmConstants.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.extension.ws.api.security.Constants;
+
+/**
+ * List of the algorithm listed on the <a href="https://www.w3.org/TR/xmlenc-core1/</a> specification and implemented by
+ * Apache WSS4J to be used for symmetric encryption.
+ */
+public enum EncryptionSymAlgorithmConstants {
+
+  TRIPLE_DES("http://www.w3.org/2001/04/xmlenc#tripledes-cbc"), AES_128("http://www.w3.org/2001/04/xmlenc#aes128-cbc"), AES_256(
+      "http://www.w3.org/2001/04/xmlenc#aes256-cbc"), AES_192("http://www.w3.org/2001/04/xmlenc#aes192-cbc"), AES_128_GCM(
+          "http://www.w3.org/2009/xmlenc11#aes128-gcm"), AES_192_GCM(
+              "http://www.w3.org/2009/xmlenc11#aes192-gcm"), AES_256_GCM("http://www.w3.org/2009/xmlenc11#aes256-gcm");
+
+  private String value;
+
+  EncryptionSymAlgorithmConstants(String value) {
+    this.value = value;
+  }
+
+  public String toString() {
+    return this.value;
+  }
+}
---
+++ src/main/java/org/mule/extension/ws/api/security/Constants/SignatureC14nAlgorithmConstants.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.extension.ws.api.security.Constants;
+
+/**
+ * List of the algorithm listed on the <a href="https://www.w3.org/TR/xmldsig-core1/</a> specification and implemented by
+ * Apache WSS4J to be used for the signature c14n (canonicalization).
+ */
+public enum SignatureC14nAlgorithmConstants {
+
+  CanonicalXML_1_0("http://www.w3.org/TR/2001/REC-xml-c14n-20010315"), CanonicalXML_1_1(
+      "http://www.w3.org/2006/12/xml-c14n11"), ExclusiveXMLCanonicalization_1_0("http://www.w3.org/2001/10/xml-exc-c14n#");
+
+
+  private String value;
+
+  SignatureC14nAlgorithmConstants(String value) {
+    this.value = value;
+  }
+
+  public String toString() {
+    return this.value;
+  }
+}
