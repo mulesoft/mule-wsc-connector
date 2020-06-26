7dd688cc784f4180ac89669751d7a9f2c1e0ebea
julian.diaz <julian.diaz@mulesoft.com>
2020-06-26 09:24:35 -0300

    WSCC-72: adds new parameter to append XML initial declaration.

--- src/main/java/org/mule/extension/ws/internal/SoapMessageBuilder.java
+++ src/main/java/org/mule/extension/ws/internal/SoapMessageBuilder.java
@@ -10,6 +10,7 @@ import org.mule.extension.ws.internal.metadata.SoapAttachmentsTypeResolver;
 import org.mule.extension.ws.internal.metadata.SoapBodyTypeResolver;
 import org.mule.extension.ws.internal.metadata.SoapHeadersTypeResolver;
 import org.mule.runtime.api.metadata.TypedValue;
+import org.mule.runtime.extension.api.annotation.Expression;
 import org.mule.runtime.extension.api.annotation.metadata.TypeResolver;
 import org.mule.runtime.extension.api.annotation.param.Content;
 import org.mule.runtime.extension.api.annotation.param.NullSafe;
@@ -19,6 +20,8 @@ import org.mule.runtime.extension.api.annotation.param.display.Summary;
 
 import java.io.InputStream;
 import java.util.Map;
+
+import static org.mule.runtime.api.meta.ExpressionSupport.NOT_SUPPORTED;
 
 /**
  * Component that specifies how to create a proper SOAP request using the {@link WebServiceConsumer}.
@@ -35,6 +38,15 @@ public class SoapMessageBuilder {
   @TypeResolver(SoapBodyTypeResolver.class)
   @Summary("The XML body to include in the SOAP message, with all the required parameters.")
   private TypedValue<InputStream> body;
+
+  /**
+   * If false, each connection will be closed after the first request is completed.
+   */
+  @Parameter
+  @Optional(defaultValue = "false")
+  @Expression(NOT_SUPPORTED)
+  @Summary("The XML body will be appended with the initial declaration.")
+  private boolean useXMLInitialDeclaration;
 
   /**
    * The XML headers to include in the SOAP message.
@@ -61,6 +73,10 @@ public class SoapMessageBuilder {
     return body;
   }
 
+  public boolean isUseXMLInitialDeclaration() {
+    return useXMLInitialDeclaration;
+  }
+
   public InputStream getHeaders() {
     return headers;
   }
@@ -69,3 +85,4 @@ public class SoapMessageBuilder {
     return attachments;
   }
 }
+
