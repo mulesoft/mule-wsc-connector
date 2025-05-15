26af0780e4ff62cf46df5ee3360f8af7041bc860
ppravallika9 <ppravallika@salesforce.com>
2025-05-15 16:42:27 +0530

    changes for java17upgrade, spring6 upgrade and min mule version upgrade

--- src/main/java/org/mule/extension/ws/internal/WebServiceConsumer.java
+++ src/main/java/org/mule/extension/ws/internal/WebServiceConsumer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * Copyright 2023 Salesforce, Inc. All rights reserved.
  * The software in this package is published under the terms of the CPAL v1.0
  * license, a copy of which has been included with this distribution in the
  * LICENSE.txt file.
@@ -35,7 +35,7 @@ import org.mule.sdk.api.annotation.JavaVersionSupport;
 @SubTypeMapping(baseType = CustomTransportConfiguration.class,
     subTypes = {CustomHttpTransportConfiguration.class, DefaultHttpTransportConfiguration.class})
 @Extension(name = NAME)
-@JavaVersionSupport(value = {JAVA_8, JAVA_11, JAVA_17})
+@JavaVersionSupport(value = {JAVA_17})
 @Xml(prefix = "wsc")
 public class WebServiceConsumer {
 
